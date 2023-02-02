d65a4b1b4d70462fcbb154a2b532ad39348fbc17
mohameds277 <mohameds277@gmail.com>
2023-02-02 02:59:01 +0200

    added the splash screen phase of the project

---
+++ travel_guide/app/src/main/java/com/example/travel_guide/sing_up_in_out.java
@@ -0,0 +1,14 @@
+package com.example.travel_guide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sing_up_in_out extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up_in_out);
+    }
+}
---
+++ travel_guide/app/src/main/java/com/example/travel_guide/splash_screen.java
@@ -0,0 +1,29 @@
+package com.example.travel_guide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class splash_screen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+       // requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splash_screen.this, MainActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+
+}
