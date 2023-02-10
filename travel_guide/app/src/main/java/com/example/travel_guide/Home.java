e82ad323f515de28c55939e5fbf95230ee894120
mohameds277 <mohameds277@gmail.com>
2023-02-10 13:11:31 +0200

    added navigation implementation

--- travel_guide/app/src/main/java/com/example/travel_guide/Home.java
+++ travel_guide/app/src/main/java/com/example/travel_guide/Home.java
@@ -1,14 +1,83 @@
 package com.example.travel_guide;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
-public class Home extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Home extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+
+
+    DrawerLayout drawer;
+    androidx.appcompat.widget.Toolbar toolbar;
+    NavigationView nag_view;
+    ActionBarDrawerToggle action_toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
+        toolbar =  findViewById(R.id.tool);
+        //setSupportActionBar(toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        nag_view = (NavigationView)findViewById(R.id.nav_view);
+        nag_view.setNavigationItemSelectedListener(this);
+
+        action_toggle = new ActionBarDrawerToggle(this , drawer, toolbar , R.string.open_nav , R.string.close_nav);
+
+
+
+        drawer.addDrawerListener(action_toggle);
+        action_toggle.syncState();
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            nag_view.setCheckedItem(R.id.nav_view);
+        }
+}
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+                break;
+            case R.id.all:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new AllFragment()).commit();
+                break;
+            case R.id.favorite:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FavFragment()).commit();
+                break;
+            case R.id.sorted:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SortedFragment()).commit();
+                break;
+            case R.id.log_out:
+                Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
--- travel_guide/app/src/main/java/com/example/travel_guide/Sign_in.java
+++ travel_guide/app/src/main/java/com/example/travel_guide/Sign_in.java
@@ -2,6 +2,7 @@ package com.example.travel_guide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ public class Sign_in extends AppCompatActivity {
     TextView email;
     TextView password;
     Button sign_in_in;
+    Intent home_activity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,9 @@ public class Sign_in extends AppCompatActivity {
         sign_in_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                home_activity = new Intent(Sign_in.this, Home.class);
+                startActivity(home_activity);
+
 
             }
         });
